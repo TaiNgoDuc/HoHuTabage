2fdd8135feb6eb0a5f04b71b59085acc8000022c
TaiNgoDuc <ngoductai0406@gmail.com>
2022-12-20 14:06:05 +0700

    up code measure distance

--- ESP_PostData/ESP_PostData.ino
+++ ESP_PostData/ESP_PostData.ino
@@ -5,13 +5,16 @@
 #include <ESP8266WiFiMulti.h>
 // #include <Servo.h>
 
-int hong_ngoai = D6;
+// int hong_ngoai = D6;
+
+const int trig = D6;     // chân trig của HC-SR04
+const int echo = D5;     // chân echo của HC-SR04
 
 // Servo myservo;
 ESP8266WiFiMulti WiFiMulti;
 
-const char* ssid = "VNUK4-2";    // Co dung thi nho doi ten wifi nhe cac nguoi anh em
-const char* password = "Z@q12wsx";   // pass luon nhe cac ban
+const char* ssid = "Kid";    // Co dung thi nho doi ten wifi nhe cac nguoi anh em
+const char* password = "taingoduc0406";   // pass luon nhe cac ban
 
 // Your IP address or domain name with URL path
 const char* serverName = "http://hohutasmartgarbage.atwebpages.com/updateData.php";
@@ -31,27 +34,62 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("Connected to WiFi");
-  pinMode(hong_ngoai, INPUT);
+  pinMode(trig, OUTPUT);
+  pinMode(echo,INPUT);
 }
 
 void loop() {
+
+  unsigned long duration; // biến đo thời gian
+  int distance;           // biến lưu khoảng cách
+
   WiFiClient client;
   HTTPClient http;
 
   String postData = "";
 
-  int sensorValue = digitalRead(hong_ngoai);
+  // int sensorValue = digitalRead(trig);
 
   Serial.print("Gia tri cam bien:");
-  Serial.println(digitalRead(hong_ngoai));  //Nếu sensor = 1 thì không phát hiện vật cản, nếu sensor = 0 thì phát hiện vật cản.
-  if (sensorValue == 0) {
-    postData = "status=Yes";
+
+  digitalWrite(trig,0);   // tắt chân trig
+  delay(2000);
+  digitalWrite(trig,1);   // phát xung từ chân trig
+  delay(5000);   // xung có độ dài 5 microSeconds
+  digitalWrite(trig,0);   // tắt chân trig
+    
+  /* Tính toán thời gian */
+  // Đo độ rộng xung HIGH ở chân echo. 
+  duration = pulseIn(echo,HIGH);  
+  // Tính khoảng cách đến vật.
+  distance = int(duration/2/29.412);
+
+
+  Serial.println(digitalRead(echo)); 
+  if (0 < distance <= 10) {
+    postData = "status=10%";
     delay(1000);
-  } else {
-    postData = "status=No";
+  } else if (10 < distance <= 20) {
+    postData = "status=20%";
+    delay(1000);
+  } else if (10 < distance <= 20) {
+    postData = "status=20%";
+    delay(1000);
+  } else if (20 < distance <= 30) {
+    postData = "status=30%";
+    delay(1000);
+  } else if (30 < distance <= 40) {
+    postData = "status=40%";
+    delay(1000);
+  } else if (50 < distance <= 60) {
+    postData = "status=50%";
+    delay(1000);
   }
 
   Serial.println(postData);
+  Serial.print(distance);
+  Serial.println("cm");
+  delay(200);
 
   http.begin(client, serverName);
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
