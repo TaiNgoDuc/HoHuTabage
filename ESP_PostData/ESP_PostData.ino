97675a1b08c3fa6dc1c55eee18d273bc2e036263
TaiNgoDuc <ngoductai0406@gmail.com>
2022-12-23 15:34:26 +0700

    fix error can not measure distance with the 2nd sensor

--- ESP_PostData/ESP_PostData.ino
+++ ESP_PostData/ESP_PostData.ino
@@ -3,13 +3,12 @@
 #include <WiFiClient.h>
 #include <Arduino_JSON.h>
 #include <ESP8266WiFiMulti.h>
-// #include <Servo.h>
 
 
-const int trig1 = D5;     // chân trig của HC-SR04
-const int echo1 = D4;     // chân echo của HC-SR04
-const int trig2 = D7;     // chân trig của HC-SR04
-const int echo2 = D6;     // chân echo của HC-SR04
+#define trig1     5   // pin TRIG to D1
+#define echo1     4   // pin ECHO to D2
+#define trig2     14   // pin TRIG to D5
+#define echo2     12   // pin ECHO to D6
 
 // Servo myservo;
 ESP8266WiFiMulti WiFiMulti;
@@ -35,9 +34,11 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("Connected to WiFi");
+
   pinMode(trig1, OUTPUT);
-  pinMode(trig2, OUTPUT);
   pinMode(echo1, INPUT);
+
+  pinMode(trig2, OUTPUT);
   pinMode(echo2, INPUT);
 }
 
@@ -58,9 +59,9 @@ void loop() {
 
   Serial.println("Gia tri cam bien:");
 
-  //Day la gia tri cua ultrasonic
+  //Day la gia tri cua ultrasonic 1
   digitalWrite(trig1,0);   // tắt chân trig
-  digitalWrite(trig2,0);   // tắt chân trig
+  
   delay(2000);
   digitalWrite(trig1,1);   // phát xung từ chân trig
   digitalWrite(trig2,1);   // phát xung từ chân trig
@@ -71,14 +72,29 @@ void loop() {
     
   /* Tính toán thời gian cua sensor */
   // Đo độ rộng xung HIGH ở chân echo. 
-  duration1 = pulseIn(echo1,HIGH);  
-  duration2 = pulseIn(echo2,HIGH); 
+  duration1 = pulseIn(echo1,HIGH);   
   // Tính khoảng cách đến vật.
   distance1 = int(duration1/2/29.412);
+
+
+  // Day la gia tri cua ultrasonic 2
+  digitalWrite(trig2,0);   // tắt chân trig
+  delay(2000);
+  digitalWrite(trig2,1);   // phát xung từ chân trig
+  delay(5000);   // xung có độ dài 5 microSeconds
+  digitalWrite(trig2,0);   // tắt chân trig
+
+    /* Tính toán thời gian cua sensor */
+  // Đo độ rộng xung HIGH ở chân echo.  
+  duration2 = pulseIn(echo2,HIGH); 
+  // Tính khoảng cách đến vật.
   distance2 = int(duration2/2/29.412);
+
+
 
   /* Tinh khoang cach trung binh cua 2 sensor */
   distance = int(( distance1 + distance2) / 2);
+
 
 
   // Serial.println(digitalRead(echo1)); 
