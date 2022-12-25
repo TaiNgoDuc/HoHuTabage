08107b7bc429d12d765bef8d1a2a426e2ef7d4fe
Huy Dang <giahuy.dng@gmail.com>
2022-12-25 10:49:37 +0700

    update trash level and bar

--- ajax_action.php
+++ ajax_action.php
@@ -5,6 +5,8 @@
     $password = "(-oN8:jr2**#Q*,g";
     $dbName = "4230494_postdata";
 
+    $trashHigh = 24;
+
     $con = new mysqli($serverName, $userName, $password, $dbName);
 
     $query_select_status = mysqli_query($con, "SELECT * FROM tbl_data WHERE id = 1");
@@ -12,8 +14,10 @@
     if(mysqli_num_rows($query_select_status)){
         while ($rows = mysqli_fetch_array($query_select_status)){
             $status = $rows['status'];
-            echo '<p>Trash Level ' . $status . '</p>';
-            
+            $trashLevel = $trashHigh - $status;
+            $trashLevelPercent = round(($trashLevel / $trashHigh) * 100);
+            echo '<span>Trash Level ' . $status . '%</span>';
+            echo '<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="' . $status . '" aria-valuemin="0" aria-valuemax="100" style="width: ' . $status . '%">' . $status . '%</div>';
         }
     }
     
